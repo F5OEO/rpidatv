6065f3bdde1f5c39413174183c5e7e6752562314
F5OEO <evaristec@gmail.com>
2016-03-06 16:10:12 +0000

    Fix install

--- install.sh
+++ install.sh
@@ -1,13 +1,7 @@
 #rpitx install
-sudo apt-get update 
-sudo apt-get install rpi-update
-sudo rpi-update
-sudo apt-get install apt-transport-https
-sudo apt-get install git
-
-git clone git://github.com/f5oeo/rpidatv
-cd rpidatv/src
+cd src
 make -j 4
+cd ..
 
 # Rtlsdr install
 sudo apt-get install cmake libusb-1.0-0-dev 
@@ -16,9 +10,10 @@ cd rtl-sdr/ && mkdir build && cd build
 cmake ../ -DINSTALL_UDEV_RULES=ON
 make && sudo make install && sudo ldconfig
 sudo bash -c 'echo -e "\n# for RTL-SDR:\nblacklist dvb_usb_rtl28xxu\n" >> /etc/modprobe.d/blacklist.conf'
+cd ..
 
 # LeanDVB install
-cd ..
+
 sudo apt-get install g++
 cd leandvb
 wget http://www.pabr.org/radio/leandvb/leandvb.cc
