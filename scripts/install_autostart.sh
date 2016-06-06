68181b57d6b471781f32e884e6d09dc3a278e6bc
F5OEO <evaristec@gmail.com>
2016-06-06 13:41:42 +0000

    Modify Autostart for Jessie

--- scripts/install_autostart.sh
+++ scripts/install_autostart.sh
@@ -1,2 +1,8 @@
-sudo cp install_inittab /etc/inittab
+#FOR Jessie
+# Whiptail and /etc/rc.local is not running properly !!!!
+# Use systemd method
+sudo mkdir -pv /etc/systemd/system/getty@tty1.service.d/
+sudo cp ./autologin.conf /etc/systemd/system/getty@tty1.service.d/
+sudo systemctl enable getty@tty1.service
+
 cp install_bashrc /home/pi/.bashrc
