8f69efcd59d9532c6f52be262648a9cbb4632234
F5OEO <evaristec@gmail.com>
2016-09-14 09:28:38 +0000

    use leandvb2video.sh instead

--- scripts/receivedatv.sh
+++
@@ -1,5 +0,0 @@
-sudo killall leandvb
-sudo killall omxplayer
-mkfifo fifots
-sudo rtl_sdr -g 20 -f 437500000 -s 1024000 - | ./leandvb --sr 249980 --anf 1 -f 1024000 |buffer > fifots &
-omxplayer fifots &
