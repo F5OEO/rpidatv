7cf3576c91fd6d63ecd0b9c424e48a044bd67b05
F5OEO <evaristec@gmail.com>
2016-09-30 08:44:30 +0000

    Add fftw3 for rpidatvgui

--- install_script_f5oeo.sh
+++ install_script_f5oeo.sh
@@ -5,7 +5,7 @@ sudo dpkg --configure -a
 sudo apt-get clean
 sudo apt-get update
 sudo apt-get -y install apt-transport-https git rpi-update
-sudo apt-get -y install cmake libusb-1.0-0-dev g++ libx11-dev buffer libjpeg8-dev indent libfreetype6-dev ttf-dejavu-core bc usbmount
+sudo apt-get -y install cmake libusb-1.0-0-dev g++ libx11-dev buffer libjpeg8-dev indent libfreetype6-dev ttf-dejavu-core bc usbmount fftw3-dev
 
 #rpi-update to get latest firmware
 sudo rpi-update
@@ -44,6 +44,7 @@ make
 cd ../
 make
 sudo make install
+
 #Menu autostart
 cd /home/pi/rpidatv/scripts/
 #make kayboard in french
