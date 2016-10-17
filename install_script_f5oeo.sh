37255fcc61a25e8a3348a8117544fbca4b77b9d5
F5OEO <evaristec@gmail.com>
2016-10-17 13:27:42 +0000

    libjpeg8->libjpeg

--- install_script_f5oeo.sh
+++ install_script_f5oeo.sh
@@ -5,7 +5,7 @@ sudo dpkg --configure -a
 sudo apt-get clean
 sudo apt-get update
 sudo apt-get -y install apt-transport-https git rpi-update
-sudo apt-get -y install cmake libusb-1.0-0-dev g++ libx11-dev buffer libjpeg8-dev indent libfreetype6-dev ttf-dejavu-core bc usbmount fftw3-dev wiringpi libvncserver-dev
+sudo apt-get -y install cmake libusb-1.0-0-dev g++ libx11-dev buffer libjpeg-dev indent libfreetype6-dev ttf-dejavu-core bc usbmount fftw3-dev wiringpi libvncserver-dev
 
 #rpi-update to get latest firmware
 sudo rpi-update
