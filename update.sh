af282ebb37baf92a9496fb8aa1edff9eb88ef40e
Dave Crump <Dave.g8gkq@gmail.com>
2016-12-16 21:42:30 +0000

    Corrected to update scripts and binaries

--- update.sh
+++ update.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-# Updated by davecrump 20161215
+# Updated by davecrump 20161216
+
+# Modified to overwrite ~/rpidatv/scripts and
+# ~/rpidatv/src, then compile
+# rpidatv, rpidatv gui avc2ts and adf4351
 
 printf "\nCommencing update.\n"
 
@@ -9,14 +13,15 @@ set -e  # Don't report errors
 # ---------- Update rpidatv -----------
 
 cd /home/pi
-wget https://github.com/davecrump/rpidatv/archive/master.zip -O master.zip
+wget -q https://github.com/davecrump/rpidatv/archive/master.zip -O master.zip
 unzip -o master.zip 
-cp -f -r rpidatv-master/bin rpidatv
-cp -f -r rpidatv-master/doc rpidatv
+# cp -f -r rpidatv-master/bin rpidatv
+# cp -f -r rpidatv-master/doc rpidatv
 cp -f -r rpidatv-master/scripts rpidatv
 cp -f -r rpidatv-master/src rpidatv
-cp -f -r rpidatv-master/video rpidatv
+# cp -f -r rpidatv-master/video rpidatv
 rm master.zip
+rm -rf rpidatv-master
 
 # Compile rpidatv core
 cd rpidatv/src
@@ -41,48 +46,48 @@ sudo make install
 cd /home/pi/rpidatv/src/adf4351
 make
 cp adf4351 ../../bin/
-cd /home/pi/rpidatv
+cd /home/pi
 
-# Get tstools
-cd /home/pi/rpidatv/src
-wget https://github.com/F5OEO/tstools/archive/master.zip
-unzip master.zip
-rm -rf tstools
-mv tstools-master tstools
-rm master.zip
+## Get tstools
+# cd /home/pi/rpidatv/src
+# wget https://github.com/F5OEO/tstools/archive/master.zip
+# unzip master.zip
+# rm -rf tstools
+# mv tstools-master tstools
+# rm master.zip
 
-# Compile tstools
-cd tstools
-make
-cp bin/ts2es ../../bin/
+## Compile tstools
+#cd tstools
+#make
+#cp bin/ts2es ../../bin/
 
-#install H264 Decoder : hello_video
-#compile ilcomponet first
-cd /opt/vc/src/hello_pi/
-sudo ./rebuild.sh
+## install H264 Decoder : hello_video
+## compile ilcomponet first
+#cd /opt/vc/src/hello_pi/
+#sudo ./rebuild.sh
 
-cd /home/pi/rpidatv/src/hello_video
-make
-cp hello_video.bin ../../bin/
+# cd /home/pi/rpidatv/src/hello_video
+# make
+#cp hello_video.bin ../../bin/
 
-# TouchScreen GUI
-# FBCP : Duplicate Framebuffer 0 -> 1
-cd /home/pi/
-wget https://github.com/tasanakorn/rpi-fbcp/archive/master.zip
-unzip master.zip
-rm -rf rpi-fbcp
-mv rpi-fbcp-master rpi-fbcp
-rm master.zip
+## TouchScreen GUI
+## FBCP : Duplicate Framebuffer 0 -> 1
+#cd /home/pi/
+#wget https://github.com/tasanakorn/rpi-fbcp/archive/master.zip
+#unzip master.zip
+#rm -rf rpi-fbcp
+#mv rpi-fbcp-master rpi-fbcp
+#rm master.zip
 
-# Compile fbcp
-cd rpi-fbcp/
-rm -rf build
-mkdir build
-cd build/
-cmake ..
-make
-sudo install fbcp /usr/local/bin/fbcp
-cd ../../
+## Compile fbcp
+#cd rpi-fbcp/
+#rm -rf build
+#mkdir build
+#cd build/
+#cmake ..
+#make
+#sudo install fbcp /usr/local/bin/fbcp
+#cd ../../
 
 # Update the version number
 rm /home/pi/rpidatv/scripts/installed_version.txt
